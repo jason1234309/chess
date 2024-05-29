9d78c84f18b98c837f97369477241e685b2b4c00
Jason Prescott <64401388+jason1234309@users.noreply.github.com>
2024-05-28 19:45:43 -0600

    service files from last semester

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,72 @@
+package service;
+
+import dataAccess.*;
+import model.AuthData;
+import model.UserData;
+
+import java.util.UUID;
+
+public class UserService {
+    UserDAO userDAOObj = new MemoryUserDAO();
+    AuthDAO authDAOObj = new MemoryAuthDAO();
+    public AuthData register(UserData user) {
+        try{
+            userDAOObj.createUser(user.getUsername(), user.getPassword(), user.getPassword());
+        }
+        catch(DataAccessException currentException){
+            return new AuthData("403 error", "Error: already taken");
+        }
+        try{
+            AuthData brandNewAuthToken = new AuthData(user.getUsername(), UUID.randomUUID().toString());
+            authDAOObj.createAuth(brandNewAuthToken.getUsername(), brandNewAuthToken.getAuthToken());
+            return brandNewAuthToken;
+        }
+        catch(DataAccessException currentException){
+            return new AuthData("not sure the erorr", "shouldnt get called");
+        }
+    }
+    public AuthData login(UserData user) {
+        // needs a try catch block
+        try{
+            if(userDAOObj.getUser(user.getUsername()) == null){
+                throw new DataAccessException("username doesnt exist");
+            }
+            UserData dataBaseUser = userDAOObj.getUser(user.getUsername());
+            if(!dataBaseUser.getPassword().equals(user.getPassword())){
+                throw new DataAccessException("Incorrect password");
+            }
+            try{
+                AuthData brandNewAuthToken = new AuthData(user.getUsername(), UUID.randomUUID().toString());
+                authDAOObj.createAuth(brandNewAuthToken.getUsername(), brandNewAuthToken.getAuthToken());
+                return brandNewAuthToken;
+            }
+            catch(DataAccessException currentException){
+                return new AuthData("not sure the erorr", "shouldnt get called");
+            }
+        }
+        catch(DataAccessException currentException){
+            return new AuthData("Auth didnt work", "shouldnt get called");
+        }
+//        try{
+//            AuthData brandNewAuthToken = new AuthData(user.getUsername(), UUID.randomUUID().toString());
+//            authDAOObj.createAuth(brandNewAuthToken.getUsername(), brandNewAuthToken.getAuthToken());
+//            return brandNewAuthToken;
+//        }
+//        catch(DataAccessException currentException){
+//            return new AuthData("not sure the erorr", "shouldnt get called");
+//        }
+    }
+    public void logout(AuthData userAuthToken) throws DataAccessException {
+        // needs a try catch block
+//        try{
+//            if(authDAOObj.getAuth(userAuthToken.getAuthToken()) == null){
+//                throw new DataAccessException("auth doesnt exist");
+//            }
+//            authDAOObj.deleteAuth(userAuthToken.getAuthToken());
+//
+//        }
+//        catch(DataAccessException currentException){
+//
+//        }
+    }
+}
