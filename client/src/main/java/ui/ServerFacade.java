65d6bdf6f088c76005bd1108d603dd868a8963fd
Jason Prescott <64401388+jason1234309@users.noreply.github.com>
2024-07-19 18:11:31 -0600

    fixed server facade not being able to handle http error codes

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -10,17 +10,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.*;
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class ServerFacade {
     private final String serverUrl;
-    private Collection<GameData> lastRecievedGameList;
+    private Collection<GameData> lastReceivedGameList;
 
     public ServerFacade(String url) {
         serverUrl = url;
     }
-    public ResponseAuth registerClient(String username, String password, String email) throws URISyntaxException, IOException {
+    public ResponseAuth registerClient(String username, String password, String email)
+            throws URISyntaxException, IOException {
         URI registerURL = new URI(serverUrl + "/user");
         HttpURLConnection registerConnection = (HttpURLConnection) registerURL.toURL().openConnection();
         registerConnection.setRequestMethod("POST");
@@ -29,35 +29,49 @@ public class ServerFacade {
         jsonObject.addProperty("username", username);
         jsonObject.addProperty("password", password);
         jsonObject.addProperty("email", email);
-        try(OutputStream requestBody = registerConnection.getOutputStream();){
+        try(OutputStream requestBody = registerConnection.getOutputStream()){
             String jsonObjectString = new Gson().toJson(jsonObject);
             requestBody.write(jsonObjectString.getBytes());
         }
         registerConnection.connect();
 
-        InputStream responseBody = registerConnection.getInputStream();
-        InputStreamReader responseReader = new InputStreamReader(responseBody);
-        return new Gson().fromJson(responseReader, ResponseAuth.class);
+        if(registerConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+            InputStream responseBody = registerConnection.getInputStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, ResponseAuth.class);
+        }else{
+            InputStream responseBody = registerConnection.getErrorStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, ResponseAuth.class);
+        }
     }
-    public ResponseAuth loginClient(String username, String password) throws URISyntaxException, IOException {
-        URI registerURL = new URI(serverUrl + "/user");
+    public ResponseAuth loginClient(String username, String password)
+            throws URISyntaxException, IOException {
+        URI registerURL = new URI(serverUrl + "/session");
         HttpURLConnection registerConnection = (HttpURLConnection) registerURL.toURL().openConnection();
         registerConnection.setRequestMethod("POST");
         registerConnection.setDoOutput(true);
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("username", username);
         jsonObject.addProperty("password", password);
-        try(OutputStream requestBody = registerConnection.getOutputStream();){
+        try(OutputStream requestBody = registerConnection.getOutputStream()){
             String jsonObjectString = new Gson().toJson(jsonObject);
             requestBody.write(jsonObjectString.getBytes());
         }
-
         registerConnection.connect();
-        InputStream responseBody = registerConnection.getInputStream();
-        InputStreamReader responseReader = new InputStreamReader(responseBody);
-        return new Gson().fromJson(responseReader, ResponseAuth.class);
+
+        if(registerConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+            InputStream responseBody = registerConnection.getInputStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, ResponseAuth.class);
+        }else{
+            InputStream responseBody = registerConnection.getErrorStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, ResponseAuth.class);
+        }
     }
-    public GameCreationResponse createClientGame(AuthData clientAuth, String gameName) throws IOException, URISyntaxException {
+    public GameCreationResponse createClientGame(AuthData clientAuth, String gameName)
+            throws IOException, URISyntaxException {
         URI registerURL = new URI(serverUrl + "/game");
         HttpURLConnection registerConnection = (HttpURLConnection) registerURL.toURL().openConnection();
         registerConnection.setRequestMethod("POST");
@@ -65,31 +79,44 @@ public class ServerFacade {
         registerConnection.addRequestProperty("authToken", clientAuth.getAuthToken());
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("gameName", gameName);
-        try(OutputStream requestBody = registerConnection.getOutputStream();){
+        try(OutputStream requestBody = registerConnection.getOutputStream()){
             String jsonObjectString = new Gson().toJson(jsonObject);
             requestBody.write(jsonObjectString.getBytes());
         }
         registerConnection.connect();
 
-        InputStream responseBody = registerConnection.getInputStream();
-        InputStreamReader responseReader = new InputStreamReader(responseBody);
-        return new Gson().fromJson(responseReader, GameCreationResponse.class);
+        if(registerConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+            InputStream responseBody = registerConnection.getInputStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, GameCreationResponse.class);
+        }else{
+            InputStream responseBody = registerConnection.getErrorStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, GameCreationResponse.class);
+        }
     }
-    public GameListResponse listServerGames(AuthData clientAuth) throws IOException, URISyntaxException {
+    public GameListResponse listServerGames(AuthData clientAuth)
+            throws IOException, URISyntaxException {
         URI registerURL = new URI(serverUrl + "/game");
         HttpURLConnection registerConnection = (HttpURLConnection) registerURL.toURL().openConnection();
-        registerConnection.setRequestMethod("POST");
+        registerConnection.setRequestMethod("GET");
         registerConnection.setDoOutput(true);
         registerConnection.addRequestProperty("authToken", clientAuth.getAuthToken());
         registerConnection.connect();
-
-        InputStream responseBody = registerConnection.getInputStream();
-        InputStreamReader responseReader = new InputStreamReader(responseBody);
-        GameListResponse gameListResponse = new Gson().fromJson(responseReader, GameListResponse.class);
-        lastRecievedGameList = gameListResponse.games();
-        return gameListResponse;
+        if(registerConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+            InputStream responseBody = registerConnection.getInputStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            GameListResponse gameListResponse = new Gson().fromJson(responseReader, GameListResponse.class);
+            lastReceivedGameList = gameListResponse.games();
+            return gameListResponse;
+        }else{
+            InputStream responseBody = registerConnection.getErrorStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, GameListResponse.class);
+        }
     }
-    public ErrorResponce joinClientToServerGame(AuthData clientAuth, int gameID, String playerColor) throws IOException, URISyntaxException {
+    public ErrorResponce joinClientToServerGame(AuthData clientAuth, int gameID, String playerColor)
+            throws IOException, URISyntaxException {
         URI registerURL = new URI(serverUrl + "/game");
         HttpURLConnection registerConnection = (HttpURLConnection) registerURL.toURL().openConnection();
         registerConnection.setRequestMethod("PUT");
@@ -98,35 +125,48 @@ public class ServerFacade {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("playerColor", playerColor.toUpperCase());
         jsonObject.addProperty("gameID", gameID);
-        try(OutputStream requestBody = registerConnection.getOutputStream();){
+        try(OutputStream requestBody = registerConnection.getOutputStream()){
             String jsonObjectString = new Gson().toJson(jsonObject);
             requestBody.write(jsonObjectString.getBytes());
         }
         registerConnection.connect();
 
-        InputStream responseBody = registerConnection.getInputStream();
-        InputStreamReader responseReader = new InputStreamReader(responseBody);
-        return new Gson().fromJson(responseReader, ErrorResponce.class);
+        if(registerConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+            InputStream responseBody = registerConnection.getInputStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, ErrorResponce.class);
+        }else{
+            InputStream responseBody = registerConnection.getErrorStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, ErrorResponce.class);
+        }
     }
     public GameData observeServerGame(int gameID){
-        for(GameData currentGame:lastRecievedGameList){
+        for(GameData currentGame: lastReceivedGameList){
             if(currentGame.getGameID() == gameID){
                 return currentGame;
             }
         }
         return null;
     }
-    public ErrorResponce logoutClient(AuthData clientAuth) throws IOException, URISyntaxException {
-        URI registerURL = new URI(serverUrl + "/game");
+    public ErrorResponce logoutClient(AuthData clientAuth)
+            throws IOException, URISyntaxException {
+        URI registerURL = new URI(serverUrl + "/session");
         HttpURLConnection registerConnection = (HttpURLConnection) registerURL.toURL().openConnection();
-        registerConnection.setRequestMethod("PUT");
+        registerConnection.setRequestMethod("DELETE");
         registerConnection.setDoOutput(true);
         registerConnection.addRequestProperty("authToken", clientAuth.getAuthToken());
         registerConnection.connect();
 
-        InputStream responseBody = registerConnection.getInputStream();
-        InputStreamReader responseReader = new InputStreamReader(responseBody);
-        return new Gson().fromJson(responseReader, ErrorResponce.class);
+        if(registerConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+            InputStream responseBody = registerConnection.getInputStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, ErrorResponce.class);
+        }else{
+            InputStream responseBody = registerConnection.getErrorStream();
+            InputStreamReader responseReader = new InputStreamReader(responseBody);
+            return new Gson().fromJson(responseReader, ErrorResponce.class);
+        }
     }
 
 }
