47efdc4f40e73294769b958b10d6daef10f7c084
Jason Prescott <64401388+jason1234309@users.noreply.github.com>
2024-08-08 15:25:29 -0600

    started the webSocketFacade

--- client/src/main/java/ui/webSocketFacade.java
+++ client/src/main/java/ui/webSocketFacade.java
@@ -1,4 +1,60 @@
 package ui;
 
-public class webSocketFacade {
+import com.google.gson.Gson;
+import responserequest.ErrorResponce;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class webSocketFacade extends Endpoint {
+    private final URI serverUri;
+    public final Session session;
+
+
+    public webSocketFacade(String url) throws URISyntaxException, DeploymentException, IOException {
+        serverUrl = url;
+        URI uri = new URI("ws://localhost:8080/ws");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.session = container.connectToServer(this, uri);
+        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+            public void onMessage(String message) {
+                System.out.println(message);
+            }
+        });
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+
+    }
+    @Override
+    public void onClose(Session session, CloseReason closeReason) {
+
+    }
+
+    @Override
+    public void onError(Session session, Throwable thr) {
+
+    }
+    public void connectClient(){
+
+    }
+
+    public void makeMoveClient(){
+
+    }
+
+    public void resignClient(){
+
+    }
+
+    public void leaveGame(){
+
+    }
+
 }
